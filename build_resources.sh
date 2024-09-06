d0221cbf9ad1be19d66320210adaf667836aa206
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-09-06 20:56:14 +0900

    add gpavatar standalone inference

--- build_resources.sh
+++ build_resources.sh
@@ -5,3 +5,5 @@ mv resources/FLAME/* ./flame_feature_extractor/renderer/assets/
 #mv resources/human_matting/* ./engines/human_matting/assets/
 mv resources/mica/* ./flame_feature_extractor/feature_extractor/mica/assets/
 #rm -r resources/
+
+wget https://github.com/xg-chu/GPAvatar/releases/download/v1.0.0/one_model.ckpt ./flame_feature_extractor/gpavatar/checkpoints/one_model.ckpt
