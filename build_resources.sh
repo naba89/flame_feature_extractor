098eb1030c94c60317bb9d25b79f55ab98371bc1
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-11-02 23:43:31 +0900

    update

--- build_resources.sh
+++ build_resources.sh
@@ -4,6 +4,6 @@ mv resources/emoca/* ./flame_feature_extractor/feature_extractor/emoca/assets/
 mv resources/FLAME/* ./flame_feature_extractor/renderer/assets/
 #mv resources/human_matting/* ./engines/human_matting/assets/
 mv resources/mica/* ./flame_feature_extractor/feature_extractor/mica/assets/
-#rm -r resources/
+rm -r resources/
 
-wget https://github.com/xg-chu/GPAvatar/releases/download/v1.0.0/one_model.ckpt ./flame_feature_extractor/gpavatar/checkpoints/one_model.ckpt
+wget https://github.com/xg-chu/GPAvatar/releases/download/v1.0.0/one_model.ckpt -O ./flame_feature_extractor/gpavatar/checkpoints/one_model.ckpt
